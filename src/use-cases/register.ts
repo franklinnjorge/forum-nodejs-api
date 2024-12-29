b3ab431341d1477ece4c2d65afbdcd4ee6e349e0
Franklin Borges <franklin-jorge@hotmail.com>
2024-12-29 14:56:45 -0500

    feat: enhance register use case to return user object in response

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,6 +1,7 @@
 import { UsersRepository } from '@/repositories/users-repository'
 import bcryptjs from 'bcryptjs'
 import { UserAlreadyExistsError } from './errors/user-already-exists-error'
+import { User } from '@prisma/client'
 
 export interface RegisterUseCaseRequest {
   name: string
@@ -8,10 +9,18 @@ export interface RegisterUseCaseRequest {
   password: string
 }
 
+export interface RegisterUseCaseResponse {
+  user: User
+}
+
 export class RegisterUseCase {
   constructor(private readonly userRepository: UsersRepository) {}
 
-  async execute({ name, email, password }: RegisterUseCaseRequest) {
+  async execute({
+    name,
+    email,
+    password,
+  }: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
     const password_hash = await bcryptjs.hash(password, 6)
     const userWithSameEmail = await this.userRepository.findByEmail(email)
 
