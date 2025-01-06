4254c063e3510318f20a7e0558e9d8b23655fbc3
Franklin Borges <franklin-jorge@hotmail.com>
2025-01-06 18:18:03 -0500

    feat: refactor server setup and update Vercel configuration

--- src/app.ts
+++ src/app.ts
@@ -92,3 +92,17 @@ app.setErrorHandler((error, request, reply) => {
     // todo Here I should log the error with datadog or sentry or any other service
   }
 })
+
+const start = async () => {
+  try {
+    await app.listen({ port: env.PORT, host: '0.0.0.0' })
+    console.log('🚀 Server running at http://localhost:3333')
+  } catch (err) {
+    app.log.error(err)
+    process.exit(1)
+  }
+}
+
+start()
+
+export default app
