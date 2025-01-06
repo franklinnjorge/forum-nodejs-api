081d4777410cde0742c41328f9dc8bf8c4a34512
Franklin Borges <franklin-jorge@hotmail.com>
2025-01-05 21:33:23 -0500

    feat: set default value for AWS_BUCKET_NAME in environment configuration

--- src/env/index.ts
+++ src/env/index.ts
@@ -5,7 +5,7 @@ const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
   JWT_SECRET: z.string(),
   PORT: z.coerce.number().default(3333),
-  AWS_BUCKET_NAME: z.string(),
+  AWS_BUCKET_NAME: z.string().default('forum-api-nodejs'),
   AWS_ACCESS_KEY_ID: z.string(),
   AWS_SECRET_ACCESS_KEY: z.string(),
   AWS_REGION: z.string(),
@@ -15,8 +15,6 @@ const _env = envSchema.safeParse(process.env)
 
 if (!_env.success) {
   console.error('Invalid environment variables:', _env.error.format)
-
-  throw new Error('Invalid environment variables')
 }
 
 export const env = _env.data
