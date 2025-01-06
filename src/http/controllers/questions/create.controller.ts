52aed2a2872bd9609c5c6772916361d3ce65c877
Franklin Borges <franklin-jorge@hotmail.com>
2025-01-06 15:40:59 -0500

    feat: enhance error handling in create question controller and update user schemas

--- src/http/controllers/questions/create.controller.ts
+++ src/http/controllers/questions/create.controller.ts
@@ -3,21 +3,28 @@ import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
 
 export async function create(request: FastifyRequest, response: FastifyReply) {
-  const createQuestionBodySchema = z.object({
-    title: z.string(),
-    content: z.string(),
-  })
+  try {
+    const createQuestionBodySchema = z.object({
+      title: z.string(),
+      content: z.string(),
+    })
 
-  const { title, content } = createQuestionBodySchema.parse(request.body)
-  const userId = request.user.sub
+    const { title, content } = createQuestionBodySchema.parse(request.body)
+    const userId = request.user.sub
 
-  const createQuestionUseCase = makeCreateQuestionUseCase()
+    const createQuestionUseCase = makeCreateQuestionUseCase()
 
-  await createQuestionUseCase.execute({
-    title,
-    content,
-    authorId: userId,
-  })
+    await createQuestionUseCase.execute({
+      title,
+      content,
+      authorId: userId,
+    })
 
-  return response.status(201).send()
+    return response.status(201).send()
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return response.status(400).send({ message: error.errors[0].message })
+    }
+    throw response.status(500).send(error)
+  }
 }
--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -1,15 +1,29 @@
+import { verifyJWT } from '@/middlewares/verify-jwt'
+import {
+  authenticateSchema,
+  createSchema,
+  profileSchema,
+  updateProfileAvatarSchema,
+} from '@/schema/users.schema'
 import { FastifyInstance } from 'fastify'
-import { register } from './register.controller'
 import { authenticate } from './authenticate.controller'
 import { profile } from './profile.controller'
-import { verifyJWT } from '@/middlewares/verify-jwt'
-import { updateProfileAvatar } from './update-profile-avatar.controller'
 import { refresh } from './refresh.controller'
+import { register } from './register.controller'
+import { updateProfileAvatar } from './update-profile-avatar.controller'
 
 export async function usersRoutes(app: FastifyInstance) {
-  app.post('/users', register)
-  app.post('/sessions', authenticate)
-  app.get('/profile', { onRequest: [verifyJWT] }, profile)
-  app.post('/profile/avatar', { onRequest: [verifyJWT] }, updateProfileAvatar)
+  app.post('/users', { schema: createSchema }, register)
+  app.post('/sessions', { schema: authenticateSchema }, authenticate)
+  app.get(
+    '/profile',
+    { onRequest: [verifyJWT], schema: profileSchema },
+    profile,
+  )
+  app.post(
+    '/profile/avatar',
+    { onRequest: [verifyJWT], schema: updateProfileAvatarSchema },
+    updateProfileAvatar,
+  )
   app.patch('/token/refresh', { onRequest: [verifyJWT] }, refresh)
 }
